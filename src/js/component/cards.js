08b3bbbccc7b3b41dbafed19a4a4d4a31cb53b73
AdrianRios25 <144917403+AdrianRios2527@users.noreply.github.com>
2024-01-26 17:03:14 +0000

    Staf

--- src/js/component/cards.js
+++ src/js/component/cards.js
@@ -1,17 +1,28 @@
 import React from "react";
 import "../../styles/home.css";
+import { Link } from "react-router-dom";
+import { BsHeart } from "react-icons/bs"; 
 
-export const Cards = () => (
+export const Cards = (props) => (
     <div className="text-center m-2">
         <div>
             <div className="card" style={{ width: "18rem" }}>
-                <img src="..." className="card-img-top" alt="..." />
+                <img src={props.image} className="card-img-top" alt="..." />
                 <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                    <h5 className="card-title text-start p-2">{props.name}</h5>
+                    <p className="card-text text-start ms-2">Gender :{props.gender}</p>
+                    <p className="card-text text-start ms-2">Hair Color :{props.hairColor}</p>
+                    <p className="card-text text-start ms-2">Eyes Color : {props.eyesColor}</p>
+                       <div className="d-flex justify-content-around">
+                            <Link to="/details">
+                                <button className="btn btn-primary">Learn More</button>
+                            </Link>
+                            <button className="btn btn-success"><BsHeart /></button> 
+                       </div>
+                        
                 </div>
             </div>
         </div>
     </div>
 );
+
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -6,12 +6,19 @@ import "../../styles/home.css";
 export const Home = () => (
 	<div className="text-center mt-5">
 		<div className="mt-5">
-			
-			<h1  className="py-5">Characters</h1>
-			
+
+			<h1 className="py-5">Characters</h1>
 			<div className="d-flex flex-wrap justify-content-around mx-4">
-				<Cards />
+				<Cards
+					name="Willy"
+					image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRL60nDFTtZoNrif00LhpIPN3YDRCEiuQ6wQ56NSht60mbz3ylRUMr7PO3pwg&s"
+					gender="Male"
+					hairColor="Brown"
+					eyesColor="Blue"
+				/>
+				
 			</div>
+
 
 		</div>
 		<div className="mt-5">
@@ -20,8 +27,8 @@ export const Home = () => (
 
 			<div className="d-flex flex-wrap justify-content-around ">
 				<Cards />
-			
-			
+
+
 			</div>
 
 		</div>
